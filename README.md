8388226443f507682db532f1581a30d94077d7b9
bigzhu <vermiliondun@gmail.com>
2017-12-25 14:28:59 +0800

    create insert sql

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # create_insert_sql
 for postgresql
+
+## use lib_py
+need clone [lib_py](https://github.com/bigzhu/lib_py) in home path
+
+##  use pipenv
+```
+pipenv install
+```
+
+## config file
+db connect info must in `./conf/db.ini`
+
+content like this
+```
+[db]
+host=127.0.0.1
+port=5432
+db_name=test
+user=test
+password=test
+```
---
+++ main.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from pathlib import Path
+home = str(Path.home())
+import sys
+sys.path.append(home + "/lib_py")
+import db_bz
+
+from sqlalchemy import text
+
+
+def getColumns(table_name, schema='public'):
+
+    sql = '''
+    SELECT column_name
+    FROM information_schema.columns
+    WHERE    upper(table_name)   =  upper('%s')
+    and table_schema = '%s'
+    order by column_name
+    ''' % (table_name, schema)
+    #print(sql)
+
+    sql = text(sql)
+
+    result = db_bz.engine.execute(sql)
+    names = []
+    for row in result:
+        names.append(row[0])
+    return names
+
+
+def createInsert(names, old_names, table_name, old_table_name):
+    names = ','.join(names)
+    old_names = ','.join(old_names)
+    sql = '''
+INSERT INTO public.%s(
+    %s)
+SELECT %s FROM public_old.%s;
+        ''' % (table_name, names, old_names, old_table_name)
+    return sql
+
+
+def main():
+    table_name = 'transfers'
+    names = getColumns(table_name)
+    old_table_name = 'transfer'
+    old_names = getColumns(old_table_name, 'public_old')
+    print(createInsert(names, old_names, table_name, old_table_name))
+
+
+if __name__ == '__main__':
+    main()
+    # import doctest
+    # doctest.testmod(verbose=False, optionflags=doctest.ELLIPSIS)
